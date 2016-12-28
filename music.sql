987c25759f99bf7f2dd0e9c5f1eeb8a57bcc6ed7
daniel gomez <danielcamilo_08@hotmail.com>
2016-12-28 08:21:55 -0500

    base de datos

--- music.sql
+++ music.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
+-- version 4.5.2
 -- http://www.phpmyadmin.net
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 26-12-2016 a las 23:12:18
--- Versión del servidor: 10.1.19-MariaDB
--- Versión de PHP: 7.0.13
+-- Servidor: localhost
+-- Tiempo de generación: 28-12-2016 a las 14:20:44
+-- Versión del servidor: 10.1.13-MariaDB
+-- Versión de PHP: 7.0.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -25,21 +25,30 @@ DELIMITER $$
 -- Procedimientos
 --
 CREATE DEFINER=`root`@`localhost` PROCEDURE `insertar` (IN `id` INT, IN `fecha` VARCHAR(100), IN `orden` INT)  BEGIN
-INSERT Into tbl_asignar_cancion (id_img,fecha,num_orden) VALUES(id,fecha,orden);
+INSERT Into TBL_ASIGNAR_CANCION (id_img,fecha,num_orden) VALUES(id,fecha,orden);
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `truncateAsignar` ()  BEGIN
+declare result int;
+TRUNCATE TBL_ASIGNAR_CANCION;
+SET result =1;
+SELECT result;
 END$$
 
 --
 -- Funciones
 --
-CREATE DEFINER=`root`@`localhost` FUNCTION `insertHistoria` () RETURNS VARCHAR(100) CHARSET latin1 begin 
+CREATE DEFINER=`root`@`localhost` FUNCTION `insertHistoria` () RETURNS VARCHAR(100) CHARSET utf8 COLLATE utf8_spanish2_ci begin 
 	declare idF int;
     declare fecha date;
     declare msn varchar(50);
-	set idF=(Select a.id_img from TBL_ASIGNAR_CANCION a order by a.id desc limit 1);
-    set fecha =(select a.fecha from TBL_ASIGNAR_CANCION a where a.id_img=idF);
+	set idF=(Select a.id from TBL_ASIGNAR_CANCION a order by a.id desc limit 1);
+    set fecha =(select a.fecha from TBL_ASIGNAR_CANCION a where a.id=idF);
 if idF >0 then
     insert into TBL_HISTORIAL_CANCION values(null,fecha,idF);
     set msn='datos ingresados';
+	ELSE	
+    set msn="null";
     end if;
     return msn;
 end$$
@@ -49,55 +58,35 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `tbl_asignar_cancion`
+-- Estructura de tabla para la tabla `TBL_ASIGNAR_CANCION`
 --
 
-CREATE TABLE `tbl_asignar_cancion` (
+CREATE TABLE `TBL_ASIGNAR_CANCION` (
   `id` int(11) NOT NULL,
   `id_img` int(11) NOT NULL,
   `fecha` date DEFAULT NULL,
   `num_orden` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
---
--- Volcado de datos para la tabla `tbl_asignar_cancion`
---
-
-INSERT INTO `tbl_asignar_cancion` (`id`, `id_img`, `fecha`, `num_orden`) VALUES
-(1, 13, '2016-12-26', 4),
-(2, 1, '2016-12-26', 3),
-(3, 7, '2016-12-26', 2),
-(4, 3, '2016-12-26', 1);
-
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `tbl_historial_cancion`
+-- Estructura de tabla para la tabla `TBL_HISTORIAL_CANCION`
 --
 
-CREATE TABLE `tbl_historial_cancion` (
+CREATE TABLE `TBL_HISTORIAL_CANCION` (
   `id_historial` int(11) NOT NULL,
   `fecha` date DEFAULT NULL,
   `id_img` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
---
--- Volcado de datos para la tabla `tbl_historial_cancion`
---
-
-INSERT INTO `tbl_historial_cancion` (`id_historial`, `fecha`, `id_img`) VALUES
-(1, '2016-12-26', 13),
-(2, '2016-12-26', 1),
-(3, '2016-12-26', 7),
-(4, '2016-12-26', 3);
-
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `tbl_img`
+-- Estructura de tabla para la tabla `TBL_IMG`
 --
 
-CREATE TABLE `tbl_img` (
+CREATE TABLE `TBL_IMG` (
   `id` int(11) NOT NULL,
   `nombre` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
   `descripcion` varchar(100) COLLATE utf8_spanish2_ci NOT NULL,
@@ -105,16 +94,16 @@ CREATE TABLE `tbl_img` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
--- Volcado de datos para la tabla `tbl_img`
+-- Volcado de datos para la tabla `TBL_IMG`
 --
 
-INSERT INTO `tbl_img` (`id`, `nombre`, `descripcion`, `direccion`) VALUES
+INSERT INTO `TBL_IMG` (`id`, `nombre`, `descripcion`, `direccion`) VALUES
 (1, 'Aclamad a Dios', 'Aclamad a Dios', 'images/thumbs/1 Aclamad a Dios.png'),
 (2, 'Alabad a jehova', 'Alabad a jehova', 'images/thumbs/2 Alabad a Jehova.png'),
 (3, 'Alabad a jehova', 'Alabad a jehova', 'images/thumbs/3 Alabad a Jehova.png'),
 (4, 'Alabare', 'Alabare', 'images/thumbs/4 Alabare.png'),
 (5, 'aunque otros canten', 'aunque otros canten', 'images/thumbs/5 Aunque otros Canten.png'),
-(6, 'Bendecid al Senor', 'Bendecid al Senor', 'images/thumbs/6 Bendecid al Senor.png'),
+(6, 'Bendecid al Señor', 'Bendecid al Señor', 'images/thumbs/6 Bendecid al Senor.png'),
 (7, 'Bendecir a Jehova', 'Bendecir a Jehova', 'images/thumbs/7 Bendecire a Jehova.png'),
 (8, 'Bendito se Jehova la Roca', 'Bendito se Jehova la Roca', 'images/thumbs/8 Bendito se Jehova la Roca.png'),
 (9, 'Bueno Es', 'Bueno Es', 'images/thumbs/9 Bueno Es.png'),
@@ -131,29 +120,30 @@ INSERT INTO `tbl_img` (`id`, `nombre`, `descripcion`, `direccion`) VALUES
 (20, 'Dios esta aqui', 'Dios esta aqui', 'images/thumbs/20 Dios esta aqui.png'),
 (21, 'Dios esta Presente', 'Dios esta Presente', 'images/thumbs/21 Dios esta Presente.png'),
 (22, 'El me ha vestido de Alegria', 'El me ha vestido de Alegria', 'images/thumbs/22 El me ha vestido de Alegria.png'),
-(23, 'El poderoso de Israel', 'El poderoso de Israel', 'images/thumbs/23 El poderoso de Israel.png');
+(23, 'El poderoso de Israel', 'El poderoso de Israel', 'images/thumbs/23 El poderoso de Israel.png'),
+(24, '', '', 'images/thumbs/');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `tbl_asignar_cancion`
+-- Indices de la tabla `TBL_ASIGNAR_CANCION`
 --
-ALTER TABLE `tbl_asignar_cancion`
+ALTER TABLE `TBL_ASIGNAR_CANCION`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_img` (`id_img`);
 
 --
--- Indices de la tabla `tbl_historial_cancion`
+-- Indices de la tabla `TBL_HISTORIAL_CANCION`
 --
-ALTER TABLE `tbl_historial_cancion`
+ALTER TABLE `TBL_HISTORIAL_CANCION`
   ADD PRIMARY KEY (`id_historial`);
 
 --
--- Indices de la tabla `tbl_img`
+-- Indices de la tabla `TBL_IMG`
 --
-ALTER TABLE `tbl_img`
+ALTER TABLE `TBL_IMG`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -161,29 +151,29 @@ ALTER TABLE `tbl_img`
 --
 
 --
--- AUTO_INCREMENT de la tabla `tbl_asignar_cancion`
+-- AUTO_INCREMENT de la tabla `TBL_ASIGNAR_CANCION`
 --
-ALTER TABLE `tbl_asignar_cancion`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `TBL_ASIGNAR_CANCION`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT de la tabla `tbl_historial_cancion`
+-- AUTO_INCREMENT de la tabla `TBL_HISTORIAL_CANCION`
 --
-ALTER TABLE `tbl_historial_cancion`
-  MODIFY `id_historial` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `TBL_HISTORIAL_CANCION`
+  MODIFY `id_historial` int(11) NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT de la tabla `tbl_img`
+-- AUTO_INCREMENT de la tabla `TBL_IMG`
 --
-ALTER TABLE `tbl_img`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+ALTER TABLE `TBL_IMG`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `tbl_asignar_cancion`
+-- Filtros para la tabla `TBL_ASIGNAR_CANCION`
 --
-ALTER TABLE `tbl_asignar_cancion`
-  ADD CONSTRAINT `tbl_asignar_cancion_ibfk_1` FOREIGN KEY (`id_img`) REFERENCES `tbl_img` (`id`) ON DELETE CASCADE;
+ALTER TABLE `TBL_ASIGNAR_CANCION`
+  ADD CONSTRAINT `tbl_asignar_cancion_ibfk_1` FOREIGN KEY (`id_img`) REFERENCES `TBL_IMG` (`id`) ON DELETE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
